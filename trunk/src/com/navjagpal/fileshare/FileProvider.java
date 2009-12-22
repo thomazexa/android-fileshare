a9e0326c5b51eab5d5b185e86ebddbf89668f494
nav <nav@8f1aa378-a533-11dd-a8c6-677c70d10fe4>
2009-12-22 16:52:30 +0000

    Using org.apache http classes instead of trying to roll my own with sockets. Allow uploading of files to the phone. Fixed some indentation.

--- trunk/src/com/navjagpal/fileshare/FileProvider.java
+++ trunk/src/com/navjagpal/fileshare/FileProvider.java
@@ -29,106 +29,108 @@ import android.provider.OpenableColumns;
 
 public class FileProvider extends ContentProvider {
 
-	public static final String AUTHORITY = "com.navjagpal.filesharer.FileProvider";
-	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY  + "/files");
-	public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.navjagpal.file";
-	public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.navjagpal.file";
+  public static final String AUTHORITY = "com.navjagpal.filesharer.FileProvider";
+  public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY  + "/files");
+  public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.navjagpal.file";
+  public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.navjagpal.file";
 
-	@Override
-	public boolean onCreate() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+  @Override
+  public boolean onCreate() {
+    // TODO Auto-generated method stub
+    return false;
+  }
 
-	@Override
-	public String getType(Uri uri) {
-		String path = getPath(uri);
-		File file = new File(path);
-		if (file.isDirectory())
-			return CONTENT_TYPE;
-		else if (file.isFile())
-			return CONTENT_ITEM_TYPE;
+  @Override
+  public String getType(Uri uri) {
+    String path = getPath(uri);
+    File file = new File(path);
+    if (file.isDirectory())
+      return CONTENT_TYPE;
+    else if (file.isFile())
+      return CONTENT_ITEM_TYPE;
 
-		return null;
-	}
+    return null;
+  }
 
-	@Override
-	public Uri insert(Uri uri, ContentValues values) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public Uri insert(Uri uri, ContentValues values) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	@Override
-	public int delete(Uri uri, String selection, String[] selectionArgs) {
-		throw new UnsupportedOperationException("Deletes are not supported.");
-	}
+  @Override
+  public int delete(Uri uri, String selection, String[] selectionArgs) {
+    throw new UnsupportedOperationException("Deletes are not supported.");
+  }
 
-	@Override
-	public Cursor query(Uri uri, String[] projection, String selection,
-			String[] selectionArgs, String sortOrder) {
+  @Override
+  public Cursor query(Uri uri, String[] projection, String selection,
+      String[] selectionArgs, String sortOrder) {
 
-		return getCursorForFiles(getPath(uri));
-	}
+    return getCursorForFiles(getPath(uri));
+  }
 
-	@Override
-	public int update(Uri uri, ContentValues values, String selection,
-			String[] selectionArgs) {
-		throw new UnsupportedOperationException("Updates are not supported.");
-	}
+  @Override
+  public int update(Uri uri, ContentValues values, String selection,
+      String[] selectionArgs) {
+    throw new UnsupportedOperationException("Updates are not supported.");
+  }
 
-	/* Add a row to the matrix cursor */
-	private void addRow(MatrixCursor cursor, File file, int id, String path) {
-		String fileName = file.getName();
-		int fileSize = (int)file.length();
-		cursor.addRow(new Object[] {id, fileName, fileSize, path});
-	}
+  /* Add a row to the matrix cursor */
+  private void addRow(MatrixCursor cursor, File file, int id, String path) {
+    String fileName = file.getName();
+    int fileSize = (int)file.length();
+    cursor.addRow(new Object[] {id, fileName, fileSize, path});
+  }
 
-	/* Return a cursor for files in the specified path */
-	private Cursor getCursorForFiles(String path) {
-		/* _id doesn't really mean anything but the system really wants it */
-		String[] columns = { 
-				"_id",
-				OpenableColumns.DISPLAY_NAME,
-				OpenableColumns.SIZE,
-				"_data"
-		};
+  /* Return a cursor for files in the specified path */
+  private Cursor getCursorForFiles(String path) {
+    /* _id doesn't really mean anything but the system really wants it */
+    String[] columns = { 
+        "_id",
+        OpenableColumns.DISPLAY_NAME,
+        OpenableColumns.SIZE,
+        "_data"
+    };
 
-		MatrixCursor c = new MatrixCursor(columns);
+    MatrixCursor c = new MatrixCursor(columns);
 
-		File baseDir = new File(path);
-		if (baseDir.isDirectory()) {
-			File[] files = baseDir.listFiles();
-			int id = 0;
-			for (File file: files) {		
-				addRow(c, file, id, file.getAbsolutePath());
-				id++;
-			}
-		} else if (baseDir.isFile()) {
-			addRow(c, baseDir, 0, baseDir.getAbsolutePath());
-		}
+    File baseDir = new File(path);
+    if (baseDir.isDirectory()) {
+      File[] files = baseDir.listFiles();
+      int id = 0;
+      for (File file: files) {		
+        addRow(c, file, id, file.getAbsolutePath());
+        id++;
+      }
+    } else if (baseDir.isFile()) {
+      addRow(c, baseDir, 0, baseDir.getAbsolutePath());
+    }
 
-		return c;
-	}
+    return c;
+  }
 
-	public ParcelFileDescriptor  openFile(Uri uri, String mode) throws FileNotFoundException, SecurityException  {
-		return openFileHelper(uri, mode);
-	}
+  @Override
+  public ParcelFileDescriptor openFile(Uri uri, String mode)
+    throws FileNotFoundException, SecurityException  {
+    return openFileHelper(uri, mode);
+  }
 
-	/* Construct a file path based on the uri */
-	public static String getPath(Uri uri) {
-		List<String> segments = uri.getPathSegments();
+  /* Construct a file path based on the uri */
+  public static String getPath(Uri uri) {
+    List<String> segments = uri.getPathSegments();
 
-		String path = "";
-		for (String segment: segments) {
-			if (segment.equals("files"))
-				continue;
-			path += "/" + segment;
-		}
-		if (path == "")
-			path = "/";
+    String path = "";
+    for (String segment: segments) {
+      if (segment.equals("files"))
+        continue;
+      path += "/" + segment;
+    }
+    if (path == "")
+      path = "/";
 
-		return path;
-	}
+    return path;
+  }
 
 
 }
