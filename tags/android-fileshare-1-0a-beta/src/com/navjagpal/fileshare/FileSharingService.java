e4a01c19aeeee5ce27a98890859214e0f664c1d7
nav <nav@8f1aa378-a533-11dd-a8c6-677c70d10fe4>
2009-12-24 15:41:26 +0000

    1.0a beta release. Long press URL sharing. File uploads.

---
+++ tags/android-fileshare-1-0a-beta/src/com/navjagpal/fileshare/FileSharingService.java
@@ -0,0 +1,93 @@
+//   Copyright 2009 Google Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package com.navjagpal.fileshare;
+
+import com.navjagpal.fileshare.IFileSharingService;
+
+import java.io.IOException;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.IBinder;
+import android.util.Log;
+
+public class FileSharingService extends Service {
+
+  static final String PREFS_NAME = "FileSharerServicePrefs";
+  static final String PREFS_ALLOW_UPLOADS = "ALLOW_UPLOADS";
+
+
+  private static final int DEFAULT_PORT = 9999;
+
+  private static final String TAG = "FileSharerService";
+
+  private WebServer mWebServer;
+
+  private int mPort;
+
+  private final IFileSharingService.Stub mBinder = new IFileSharingService.Stub() {
+    public int getPort() {
+      return mPort;
+    }
+  };
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+    mPort = settings.getInt("port", DEFAULT_PORT);
+    try {
+      mWebServer = new WebServer(
+          getContentResolver(), settings, mPort);
+      mWebServer.setOnTransferStartedListener(new WebServer.TransferStartedListener() {
+        public void started(Uri uri) {
+          NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+          Notification notification = new Notification(R.drawable.folder, null, System.currentTimeMillis());
+          PendingIntent pendingIntent = PendingIntent.getActivity(FileSharingService.this, 0, new Intent(
+              FileSharingService.this, FileShare.class), 0);
+          notification.setLatestEventInfo(FileSharingService.this, "File Share", "Transfer Started", pendingIntent);
+          nm.notify(1, notification);
+        }
+      });
+    } catch (IOException e) {
+      Log.e(TAG, "Problem creating server socket " + e.toString());
+    }
+
+    Thread t = new Thread() {
+      @Override
+      public void run() {
+        mWebServer.runWebServer();
+      }
+    };
+    t.start();
+    Log.i(TAG, "Started webserver");
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    if (IFileSharingService.class.getName().equals(intent.getAction())) {
+      return mBinder;
+    }
+    return null;
+  }
+
+}
---
+++ tags/android-fileshare-1-0a-beta/src/com/navjagpal/fileshare/WebServer.java
@@ -0,0 +1,384 @@
+//   Copyright 2009 Google Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package com.navjagpal.fileshare;
+
+import org.apache.commons.fileupload.MultipartStream;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.RequestLine;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.DefaultHttpServerConnection;
+import org.apache.http.message.BasicHttpEntityEnclosingRequest;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.params.BasicHttpParams;
+
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URLDecoder;
+
+import android.content.ContentResolver;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.OpenableColumns;
+import android.util.Log;
+
+public class WebServer {
+
+  private static final String TAG = "FileSharer WebServer";
+
+  private int mPort;
+
+  private ServerSocket mServerSocket;
+
+  private ContentResolver mContentResolver;
+  
+  private SharedPreferences mSharedPreferences;
+
+  public interface TransferStartedListener {
+    public void started(Uri uri);
+  }
+
+  private TransferStartedListener mTransferStartedListener;
+
+  /* Start the webserver on specified port */
+  public WebServer(
+      ContentResolver contentResolver,
+      SharedPreferences sharedPreferences,
+      int port) throws IOException {
+    mPort = port;
+    mServerSocket = new ServerSocket(mPort);
+    mServerSocket.setReuseAddress(true);
+    mContentResolver = contentResolver;
+    mSharedPreferences = sharedPreferences;
+  }
+
+  /* Returns port we're using */
+  public int getPort() {
+    return mPort;
+  }
+
+  public void setOnTransferStartedListener(TransferStartedListener listener) {
+    mTransferStartedListener = listener;
+  }
+
+  public void runWebServer() {
+    while (true) {
+      Log.i(TAG, "Running main webserver thread");
+      try {
+        final Socket socket = mServerSocket.accept();
+        Log.d(TAG, "Socket accepted");
+        Thread t = new Thread() {
+          @Override
+          public void run() {
+            handleRequest(socket);
+          }
+        };
+        t.start();
+      } catch (IOException e) {
+        Log.e(TAG, "Problem accepting socket " + e.toString());
+      }
+    }
+  }
+
+  /* Handles a single request. */
+  public void handleRequest(Socket socket) {
+    try {
+      DefaultHttpServerConnection serverConnection = 
+        new DefaultHttpServerConnection();
+      serverConnection.bind(socket, new BasicHttpParams());
+      HttpRequest request = serverConnection.receiveRequestHeader();
+      RequestLine requestLine = request.getRequestLine();
+
+      if (requestLine.getUri().equals("/")) {
+        Log.i(TAG, "Sending shared folder listing");
+        sendSharedFolderListing(serverConnection);
+      } else if (requestLine.getMethod().equals("GET") &&
+          requestLine.getUri().startsWith("/folder")) {
+        Log.i(TAG, "Sending list of shared files");
+        sendSharedFilesList(serverConnection, requestLine);
+      } else if (requestLine.getUri().startsWith("/file")) {
+        Log.i(TAG, "Sending file content");
+        sendFileContent(serverConnection, requestLine); 
+      } else if (requestLine.getMethod().equals("POST")) {
+        Log.i(TAG, "User is uploading file");
+        handleUploadRequest(serverConnection, request, requestLine);
+      } else {
+        Log.i(TAG, "No action for " + requestLine.getUri());
+        sendNotFound(serverConnection);
+      }
+      serverConnection.flush();
+      serverConnection.close();
+    } catch (IOException e) {
+      Log.e(TAG, "Problem with socket " + e.toString());
+    } catch (HttpException e) {
+      Log.e(TAG, "Problemw with HTTP server " + e.toString());
+    }
+  }
+
+  private void sendNotFound(DefaultHttpServerConnection serverConnection)
+  throws UnsupportedEncodingException, HttpException, IOException {
+    HttpResponse response = new BasicHttpResponse(
+        new HttpVersion(1, 1), 404, "NOT FOUND");
+    response.setEntity(new StringEntity("NOT FOUND"));
+    serverConnection.sendResponseHeader(response);
+    serverConnection.sendResponseEntity(response);
+  }
+
+  private void handleUploadRequest(DefaultHttpServerConnection serverConnection,
+      HttpRequest request, RequestLine requestLine)
+  throws IOException, HttpException, UnsupportedEncodingException {
+    HttpResponse response = new BasicHttpResponse(
+        new HttpVersion(1, 1), 200, "OK");
+    String folderId = getFolderId(requestLine.getUri());
+    processUpload(folderId, request, serverConnection);
+    String header = getHTMLHeader();
+    String form = getUploadForm(folderId);
+    String footer = getHTMLFooter();
+    String listing = getFileListing(
+        Uri.withAppendedPath(
+            FileSharingProvider.Folders.CONTENT_URI, folderId));
+    response.setEntity(new StringEntity(
+        header + listing + form + footer));
+    serverConnection.sendResponseHeader(response);
+    serverConnection.sendResponseEntity(response);
+  }
+
+  private void sendFileContent(DefaultHttpServerConnection serverConnection,
+      RequestLine requestLine)
+  throws IOException, HttpException {
+    HttpResponse response = new BasicHttpResponse(
+        new HttpVersion(1, 1), 200, "OK");
+    String fileId = getFileId(requestLine.getUri());
+    addFileEntity(
+        Uri.withAppendedPath(FileSharingProvider.Files.CONTENT_URI,
+            fileId),
+            response);
+    serverConnection.sendResponseHeader(response);
+    serverConnection.sendResponseEntity(response);
+  }
+
+  private void sendSharedFilesList(DefaultHttpServerConnection serverConnection,
+      RequestLine requestLine) throws UnsupportedEncodingException,
+      HttpException, IOException {
+    HttpResponse response = new BasicHttpResponse(
+        new HttpVersion(1, 1), 200, "OK");
+    String folderId = getFolderId(requestLine.getUri());
+    String header = getHTMLHeader();
+    String form = getUploadForm(folderId);
+    String footer = getHTMLFooter();
+    String listing = getFileListing(
+        Uri.withAppendedPath(
+            FileSharingProvider.Folders.CONTENT_URI, folderId));
+    response.setEntity(new StringEntity(
+        header + listing + form + footer));
+    serverConnection.sendResponseHeader(response);
+    serverConnection.sendResponseEntity(response);
+  }
+
+  private void sendSharedFolderListing(
+      DefaultHttpServerConnection serverConnection)
+  throws UnsupportedEncodingException, HttpException, IOException {
+    HttpResponse response = new BasicHttpResponse(
+        new HttpVersion(1, 1), 200, "OK");
+    response.setEntity(new StringEntity(
+        getHTMLHeader() + getFolderListing() + getHTMLFooter()));
+    serverConnection.sendResponseHeader(response);
+    serverConnection.sendResponseEntity(response);
+  }
+
+  @SuppressWarnings("deprecation")
+  public void processUpload(
+      String folderId,
+      HttpRequest request,
+      DefaultHttpServerConnection serverConnection)
+  throws IOException, HttpException {
+
+    /* Find the boundary and the content length. */
+    String contentType = request.getFirstHeader("Content-Type").getValue();
+    String boundary = contentType.substring(
+        contentType.indexOf("boundary=") + "boundary=".length());
+    long sizeBytes = Long.parseLong(
+        request.getFirstHeader("Content-Length").getValue());
+    BasicHttpEntityEnclosingRequest enclosingRequest =
+      new BasicHttpEntityEnclosingRequest(request.getRequestLine());
+    serverConnection.receiveRequestEntity(enclosingRequest);
+
+    InputStream input = enclosingRequest.getEntity().getContent();
+    MultipartStream multipartStream = new MultipartStream(
+        input, boundary.getBytes());
+    String headers = multipartStream.readHeaders();
+
+    /* Get the filename. */
+    StringTokenizer tokens = new StringTokenizer(
+        headers, ";", false);
+    String filename = null;
+    while (tokens.hasMoreTokens() && filename == null) {
+      String token = tokens.nextToken().trim();
+      if (token.startsWith("filename=")) {
+        filename = URLDecoder.decode(token.substring(
+            "filename=\"".length(), token.lastIndexOf("\"")), "utf8");
+      }
+    }
+
+    File uploadDirectory = new File("/sdcard/fileshare/uploads");
+    if (!uploadDirectory.exists()) {
+      uploadDirectory.mkdirs();
+    }
+
+    /* Write the file and add it to the shared folder. */
+    File uploadFile = new File(uploadDirectory, filename);
+    FileOutputStream output = new FileOutputStream(uploadFile);
+    multipartStream.readBodyData(output);
+    output.close(); 
+
+    Uri fileUri = Uri.withAppendedPath(
+        FileProvider.CONTENT_URI,
+        uploadFile.getAbsolutePath());
+    Uri folderUri = Uri.withAppendedPath(
+        FileSharingProvider.Folders.CONTENT_URI, folderId);
+    FileSharingProvider.addFileToFolder(
+        mContentResolver, fileUri, folderUri);
+  }
+
+  public String getHTMLHeader() {
+    return "<html><head><title>File Share</title></head><body>";
+  }
+
+  public String getHTMLFooter() {
+    return "</body></html>";
+  }
+
+  private String getFolderListing() {
+    /* Get list of folders */
+    Cursor c = mContentResolver.query(
+        FileSharingProvider.Folders.CONTENT_URI, null, null, null, null);
+    int nameIndex = c.getColumnIndexOrThrow(
+        FileSharingProvider.Folders.Columns.DISPLAY_NAME);
+    int idIndex = c.getColumnIndexOrThrow(
+        FileSharingProvider.Folders.Columns._ID);
+    String s = "";
+    while (c.moveToNext()) {
+      String name = c.getString(nameIndex);
+      int id = c.getInt(idIndex);
+      s += folderToLink(name, id) + "<br/>";
+    }	
+    return s;
+  }
+
+  /**
+   * Returns a form that allows users to upload files.
+   */
+  private String getUploadForm(String folderId) {
+    if (mSharedPreferences.getBoolean(
+        FileSharingService.PREFS_ALLOW_UPLOADS, false)) {
+      return"<form method=\"POST\" action=\"/folder/" + folderId + "\" " + 
+      "enctype=\"multipart/form-data\"> " +
+      "<input type=\"file\" name=\"file\" size=\"40\"/> " +
+      "<input type=\"submit\" value=\"Upload\"/>";
+    }
+    return "";
+  }
+
+  private String getFolderId(String firstline) {
+    Pattern p = Pattern.compile("/folder/(\\d+)");
+    Matcher m = p.matcher(firstline);
+    boolean b = m.find(0);
+    if (b) {
+      return m.group(1);
+    }
+    return null;
+  }
+
+  private String getFileId(String firstline) {
+    Pattern p = Pattern.compile("/file/(\\d+)");
+    Matcher m = p.matcher(firstline);
+    boolean b = m.find(0);
+    if (b) {
+      return m.group(1);
+    }
+    return null;
+  }
+
+  private String getFileListing(Uri uri) {
+    int folderId = Integer.parseInt(uri.getPathSegments().get(1));
+    Uri fileUri = FileSharingProvider.Files.CONTENT_URI;
+    String where = FileSharingProvider.Files.Columns.FOLDER_ID + "=" + folderId;
+    Cursor c = mContentResolver.query(fileUri, null, where, null, null);
+    int nameIndex = c.getColumnIndexOrThrow(
+        FileSharingProvider.Files.Columns.DISPLAY_NAME);
+    int idIndex = c.getColumnIndexOrThrow(
+        FileSharingProvider.Files.Columns._ID);
+    String s = "";
+    while (c.moveToNext()) {
+      String name = c.getString(nameIndex);
+      int id = c.getInt(idIndex);
+      s += fileToLink(name, id) + "<br/>";
+    }
+    return s;
+  }
+
+  private void addFileEntity(final Uri uri, HttpResponse response)
+  throws IOException {
+    if (mTransferStartedListener != null) {
+      mTransferStartedListener.started(uri);
+    }
+
+    Cursor c = mContentResolver.query(uri, null, null, null, null);
+    c.moveToFirst();
+    int nameIndex = c.getColumnIndexOrThrow(
+        FileSharingProvider.Files.Columns.DISPLAY_NAME);
+    String name = c.getString(nameIndex);
+    int dataIndex = c.getColumnIndexOrThrow(
+        FileSharingProvider.Files.Columns._DATA);
+    Uri data = Uri.parse(c.getString(dataIndex));
+
+    c = mContentResolver.query(data, null, null, null, null);
+    c.moveToFirst();
+    int sizeIndex = c.getColumnIndexOrThrow(OpenableColumns.SIZE);
+    int sizeBytes = c.getInt(sizeIndex);
+
+    InputStream input = mContentResolver.openInputStream(data);
+
+    String contentType = "application/octet-stream";
+    if (name.endsWith(".jpg")) {
+      contentType= "image/jpg";
+    }
+
+    response.addHeader("Content-Type", contentType);
+    response.addHeader("Content-Length", "" + sizeBytes);
+    response.setEntity(new InputStreamEntity(
+        input, sizeBytes));
+  }
+
+  private String folderToLink(String folderName, int folderId) {
+    return "<a href=\"/folder/" + folderId + "\">" + folderName + "</a>";
+  }
+
+  private String fileToLink(String fileName, int fileId) {
+    return "<a href=\"/file/" + fileId + "/" + fileName + "\">" + fileName + "</a>";
+  }
+}
